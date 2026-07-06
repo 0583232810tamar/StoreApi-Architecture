f936fe1b7675092299a792a42b72787e082e5ea2
tamarmargi <tamar48719@gmail.com>
2026-07-06 23:47:37 +0300

    fix bags, rabbitMQ, getWay

--- Microservices/ApiGateway/Program.cs
+++ Microservices/ApiGateway/Program.cs
@@ -12,7 +12,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiGateway v1");
+        options.SwaggerEndpoint("/swagger/product-catalog/v1/swagger.json", "Product Catalog Service");
+        options.SwaggerEndpoint("/swagger/orders/v1/swagger.json", "Orders Service");
+        options.SwaggerEndpoint("/swagger/bff/v1/swagger.json", "BFF Service");
+        options.SwaggerEndpoint("/swagger/storeapi/v1/swagger.json", "StoreApi Core & Auth");
+        options.SwaggerEndpoint("/swagger/notification/v1/swagger.json", "Notification Service");
+    });
 }
 
 app.MapGet("/health", () => Results.Ok(new { service = "ApiGateway", status = "Healthy" }));
